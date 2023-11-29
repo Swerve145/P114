67542a365e357a52900d8bab0b0ad73cb21774d4
Sahishnu Jadhav <96820614+Swerve145@users.noreply.github.com>
2023-11-29 17:44:45 +0000

    Add files via upload

---
+++ P114/test1.py
@@ -0,0 +1,9 @@
+a = 33
+b = "33"
+
+if b>a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+## Gives error because the data types are not the same hence can't compare.
---
+++ P114/test2.py
@@ -0,0 +1,8 @@
+def my_function(f_name):
+    print(f_name + "Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linues")
+
+## No errors it will print the real thing as expected - no spaces however
